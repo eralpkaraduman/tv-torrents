7e00b67feb5204578fe33377a74d10d5bf8b884d
Eralp Karaduman <eralpkaraduman@gmail.com>
2017-12-14 23:48:07 +0200

    added show selection prompt where multiple shows were found for query

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
 const program = require('commander');
 const colors = require('colors');
 const utils = require('./utils');
+const inquirer = require('inquirer');
 
-module.exports = function() {
-	program
-		.version('1.0.0')
-		.usage('[options] <Series Name>')
-		.option('-s, --season [number]', 'Season Number', 1)
-		.parse(process.argv);
+program
+	.version('1.0.0')
+	.usage('[options] <Series Name>')
+	.option('-s, --season [number]', 'Season Number', 1)
+	.parse(process.argv);
 
-	const [name] = program.args;
-	const {season} = program;
+const [name] = program.args;
+const { season } = program;
 
-	if (!name) {
-		console.error(colors.red('Error: No series name provided'));
-		program.outputHelp();
-		process.exit(1);
-	}
+if (!name) {
+	console.error(colors.red('Error: No series name provided'));
+	program.outputHelp();
+	process.exit(1);
+}
 
-	utils.getMagnets(name, season)
+utils.getShows(name)
+	.then(options => {
+		if (options.length > 1) {
+			return inquirer.prompt([{
+				message: 'Multiple shows found, choose one',
+				name: 'selectedShow',
+				type: 'list',
+				choices: options.map(option => option.title)
+			}])
+				.then(answers => ({ selectedShow: answers.selectedShow, options }));
+		}
+		else if (options.length === 1) {
+			return { selectedShow: options[0].title, options }
+		}
+		else {
+			throw 'No shows found';
+		}
+	})
+	.then(({ selectedShow, options }) => {
+		const [selectedOption] = options.filter(option => {
+			return option.title === selectedShow;
+		});
+		const { data } = selectedOption;
+		return data;
+	})
+	.then(data => utils.getMagnetsOfShow(data, season))
 	.then(magnets => magnets.reduce((text, magnet) => text += magnet + '\n\n', ''))
 	.then(text => utils.pbcopy(text))
 	.then(() => console.log('Copied magnet links to clipboard!'))
@@ -26,4 +51,3 @@ module.exports = function() {
 		console.error(colors.red(`Error: ${err}`));
 		process.exit(1);
 	});
-}
--- utils.js
+++ utils.js
@@ -2,56 +2,58 @@ const EztvApi = require('eztv-api-pt');
 const R = require('ramda');
 
 const eztv = new EztvApi({
-  baseUrl: 'https://eztv.ag/', // The base url of eztv. Defaults to 'https://eztv.ag/'.
-  debug: true // Show extra output. Defaults to 'false'.
+	baseUrl: 'https://eztv.ag/', // The base url of eztv. Defaults to 'https://eztv.ag/'.
+	debug: true // Show extra output. Defaults to 'false'.
 });
 
-const getMagnets = (show, season = 1) => {
-  return eztv.getAllShows()
-  .then(res => {
-    const datas = res.filter(data => R.contains(show, data.show));
-    console.log('Found shows: ' + datas.map(data => data.show));
-    const [data] = datas;
-    console.log('Using show: ' + data.show);
-    return eztv.getShowData(data);
-  })
-  .then(res => {
-    const seasonData = res.episodes[String(season)];
-    if (seasonData) {
-      console.log('Found season: ' + season);
-      return seasonData;
-    }
-    else {
-      throw 'Season ' + season + ' not found!';
-    }
-  })
-  .then(seasonData => R.toPairs(seasonData).map(
-    pair => ({number: pair[0], data: pair[1]})
-  ))
-  .then(episodes => episodes.map(episode => {
-    const bestQuality = R.head(R.keys(episode.data).sort(
-      (a, b) => parseInt(a) <= parseInt(b)
-    ));
-    console.log(episode.number + ' -> ' + bestQuality);
-    return episode.data[bestQuality].url;
-  }));
+const getShows = (show) => new Promise((resolve, reject) => {
+	return eztv.getAllShows()
+		.then(res => {
+			const datas = res.filter(data => R.contains(show, data.show));
+			return resolve(datas.map(data => ({ title: data.show, data })));
+		})
+		.catch(err => reject(err));
+});
+
+const getMagnetsOfShow = (showData, season = 1) => {
+	return eztv.getShowData(showData).then(res => {
+		const seasonData = res.episodes[String(season)];
+		if (seasonData) {
+			console.log('Found season: ' + season);
+			return seasonData;
+		}
+		else {
+			throw 'Season ' + season + ' not found!';
+		}
+	})
+		.then(seasonData => R.toPairs(seasonData).map(
+			pair => ({ number: pair[0], data: pair[1] })
+		))
+		.then(episodes => episodes.map(episode => {
+			const bestQuality = R.head(R.keys(episode.data).sort(
+				(a, b) => parseInt(a) <= parseInt(b)
+			));
+			console.log(episode.number + ' -> ' + bestQuality);
+			return episode.data[bestQuality].url;
+		}));
 }
 
 function pbcopy(data) {
-  return new Promise(function(resolve, reject) {
-    const proc = require('child_process').spawn('pbcopy');
-    proc.on('error', function(err) {
-      reject(err);
-    });
-    proc.on('close', function(err) {
-      resolve();
-    });
-    proc.stdin.write(data);
-    proc.stdin.end();
-  })
+	return new Promise(function (resolve, reject) {
+		const proc = require('child_process').spawn('pbcopy');
+		proc.on('error', function (err) {
+			reject(err);
+		});
+		proc.on('close', function (err) {
+			resolve();
+		});
+		proc.stdin.write(data);
+		proc.stdin.end();
+	})
 };
 
 module.exports = {
-	getMagnets,
+	getShows,
+	getMagnetsOfShow,
 	pbcopy
 };
